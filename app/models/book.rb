41f3e60dd56ca324c6159e81b78103319e70d069
Scott Firestone <scott.firestone.dev@gmail.com>
2016-08-28 12:26:18 -0600

    Add relationships for book and location
    Book has one location, and one location slot through location
    Location has many location slots, and many books through location slots

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ApplicationRecord
+  has_one :location_slot
+  has_one :location, through: :location_slot
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,4 @@
 class Location < ApplicationRecord
+  has_many :location_slots
+  has_many :books, through: :location_slots
 end
