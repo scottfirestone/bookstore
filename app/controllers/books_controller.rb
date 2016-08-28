c598ea86650160d15f63781cf17a89c46c9eac3f
Scott Firestone <scott.firestone.dev@gmail.com>
2016-08-28 11:37:11 -0600

    Add create action and strong params to books controllergs

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,15 @@
 class BooksController < ApplicationController
   def new
-
+    @book = Book.new
   end
+
+  def create
+    @book = Book.new(book_params)
+  end
+
+  private
+
+    def book_params
+      params.require(:book).permit(:author, :title)
+    end
 end
